4833267af3b1a3dddcf59d65fb87bfeff013e53b
Your Name <blake.jennings@gmail.com>
2014-01-18 15:18:37 -0800

    figaro, admin creds

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'figaro'
+
 gem 'rails', '4.0.1'
 
 # Use sqlite3 as the database for Active Record
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,6 @@
 class VisitorsController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PW"]
+                                      #Figaro.env.ADMIN_NAME works, too, but Bookis likes the other way
 
   def index
   end
